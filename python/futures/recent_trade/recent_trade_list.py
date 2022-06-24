7aebcfed6762e3b718bd62a75f40868ef46ba7ea
comolli <liangweicheng2@gmail.com>
2022-06-24 16:18:35 +0800

    fix domian.

--- python/futures/recent_trade/recent_trade_list.py
+++ python/futures/recent_trade/recent_trade_list.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "http://127.0.0.1:8000/fexgate/v1/fbinance/recent_trades?symbol=btcusdt&limit=10"
+url = "http://exgate.leafan.cc/api/fexgate/v1/fbinance/recent_trades?symbol=btcusdt&limit=10"
 
 payload={}
 headers = {}
